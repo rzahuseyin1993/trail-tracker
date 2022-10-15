0184856b614cd9f79e5675a7a65211ed09b82a38
rzahuseyin1993 <rzahuseyin1993@gmail.com>
2022-10-15 14:06:51 +0400

    fix multiple trail lines

--- api/index.php
+++ api/index.php
@@ -13,27 +13,35 @@ $method = $_SERVER['REQUEST_METHOD'];
 if ($method == 'GET') {
     if (isset($_GET["type"]) && $_GET["type"] == 'trail') {
         $trailRows = csvToArray("data/trail_lines.csv");
-        echo json_encode($trailRows);
+        $results = array();
+        foreach ($trailRows as $row) {
+            $results[$row[1]][] = $row;
+        }
+        echo json_encode($results);
     } elseif (isset($_GET["type"]) && $_GET["type"] == 'tracker') {
         $bufferSize = 50;
         if (isset($_GET["buffer"])) {
             $bufferSize = (float) $_GET['buffer'];
         }
         $trailRows = csvToArray("data/trail_lines.csv");
-        $line = array();
-        foreach ($trailRows as $row) {
-            $line[] = array((float) $row[3], (float) $row[4]);
-        }
         $trackerRows = csvToArray("data/tracker_logs.csv");
+
+        $lines = array();
+        foreach ($trailRows as $row) {
+            $lines[$row[1]][] = array((float) $row[3], (float) $row[4]);
+        }
 
         $geospatial = new GeoSpatial();
         if (isset($_GET["class"]) && $_GET["class"] == 'inside') {
             $results = array();
             foreach ($trackerRows as $row) {
                 $point = array((float) $row[7], (float) $row[6]);
-                $dist = $geospatial->pointToLineDistance($point, $line, "meters");
-                if ($dist <= $bufferSize) {
-                    $results[] = $row;
+                foreach ($lines as $key => $line) {
+                    $dist = $geospatial->pointToLineDistance($point, $line, "meters");
+                    if ($dist <= $bufferSize) {
+                        $results[] = $row;
+                        break;
+                    }
                 }
             }
             echo json_encode($results);
@@ -41,8 +49,14 @@ if ($method == 'GET') {
             $results = array();
             foreach ($trackerRows as $row) {
                 $point = array((float) $row[7], (float) $row[6]);
-                $dist = $geospatial->pointToLineDistance($point, $line, "meters");
-                if ($dist > $bufferSize) {
+                $flag = 0;
+                foreach ($lines as $key => $line) {
+                    $dist = $geospatial->pointToLineDistance($point, $line, "meters");
+                    if ($dist <= $bufferSize) {
+                        $flag = 1;
+                    }
+                }
+                if ($flag == 0) {
                     $results[] = $row;
                 }
             }
